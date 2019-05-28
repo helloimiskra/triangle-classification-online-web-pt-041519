c51904261240d3d45f9423bd3d2915ec84164716
Iskra B <ibatistapoblete@gmail.com>
2019-05-28 07:33:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,9 @@ class Triangle
   end
 
   def kind
-    if @length_one < 0 || @length_two < 0 || @length_three < 0
+    if @length_one <= 0 || @length_two <= 0 || @length_three <= 0
+      raise TriangleError
+    elsif @length_one + @length_two > @length_three || @length_one + @length_three > @length_two || @length_two + @length_three > @length_one
       raise TriangleError
     elsif @length_one == @length_two && @length_two == @length_three
       return :equilateral
@@ -22,7 +24,7 @@ class Triangle
 
   class TriangleError < StandardError
   end
-
+  
 end
 
 
