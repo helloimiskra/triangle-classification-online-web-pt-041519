8576abe22e9bcd7241e3469fb231d930f7b1c1b6
Iskra B <ibatistapoblete@gmail.com>
2019-05-28 07:00:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
+  attr_accessor :equilateral, :isosceles, :scalene, :length
+
+  def initialize(length, length, length)
+    @length = length
+  end
+
+  def kind
+
+  end
+
+  def initialize
   # write code here
+ #equalateral triangle - all lengths are same
+    #isosceles - 2 lengths are same, 1 different
+    #scalene - lengths are all different, sum of two lengths always exceeds 3rd
+    #each length is bigger than 0
+    
+
+
 end
